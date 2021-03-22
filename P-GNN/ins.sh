51712a1c0d155ad440003c2dfa4f279ae7b18ba0
amokvarma <amokhvarma@gmail.com>
2021-03-22 12:20:15 +0530

    networkx problem corrected

---
+++ P-GNN/ins.sh
@@ -0,0 +1,14 @@
+pip uninstall torch-scatter
+pip uninstall torch-sparse
+pip uninstall torch-cluster
+pip uninstall torch-spline-conv
+pip uninstall torch
+pip install torch
+pip install torchvision
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.8.0+cu101.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.8.0+cu101.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.8.0+cu101.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.8.0+cu101.html
+pip install torch-geometric
+unzip data/ppi.zip -d data/
+tensorboardx
