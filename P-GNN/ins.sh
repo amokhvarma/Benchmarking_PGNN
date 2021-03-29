3c3e2dce26d6fe70ad52391bf3eec3135ca90b54
amokvarma <amokhvarma@gmail.com>
2021-03-29 10:53:30 +0530

    Torch version included

--- P-GNN/ins.sh
+++ P-GNN/ins.sh
@@ -3,8 +3,7 @@ pip uninstall torch-sparse
 pip uninstall torch-cluster
 pip uninstall torch-spline-conv
 pip uninstall torch
-pip install torch
-pip install torchvision
+pip install torch==1.8.0+cu101 torchvision==0.9.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.8.0+cu101.html
 pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.8.0+cu101.html
 pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.8.0+cu101.html
